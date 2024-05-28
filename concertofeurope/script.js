744029c46dde22a3053ca5a2b63672de183118f3
eli-santaella <167122274+eli-santaella@users.noreply.github.com>
2024-05-28 01:39:24 -0700

    Delete concertofeurope directory

--- concertofeurope/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.merch-item').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.border = "3px solid #ff0000";
-    });
-
-    item.addEventListener('mouseout', () => {
-        item.style.border = "none";
-    });
-});
